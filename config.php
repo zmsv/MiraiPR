04e3ffc02a081115868db02138f91846055c7e19
筑梦师V2.0 <2967713804@qq.com>
2022-10-29 10:25:22 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+define("http","http://localhost:5700"); //http api
+
+define("verifyKey","JCIOAPABCBS"); //verifyKey
+
+define("Authorization","bearer"); //Authorization
+
+define("admin_qq",); //admin_qq
+
+define("admin_group",); //admin_group
+?>
